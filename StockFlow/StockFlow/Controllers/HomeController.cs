022c1192e0201f76e5d730c4137b76be513c6a97
VladislavTsolov <vladislavtsolov1337@gmail.com>
2026-09-17 16:58:26 +0300

    Added Entity Framework Core and SQL Server to project

--- StockFlow/StockFlow/Controllers/HomeController.cs
+++ StockFlow/StockFlow/Controllers/HomeController.cs
@@ -23,6 +23,12 @@ namespace StockFlow.Controllers
             return View();
         }
 
+        public IActionResult About()
+        {
+            ViewBag.Message = "The beginning of my app.";
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
